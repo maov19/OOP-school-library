ef394c395f1ebc8472fb8e8bbe6ab7cff7227f4c
mossa <martin.ossa.8@gmail.com>
2023-05-18 10:52:20 +0200

    Update person file

--- person.rb
+++ person.rb
@@ -2,14 +2,15 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     super()
     @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
