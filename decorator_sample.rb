7cce805abe689e6918677d7e2354a6038e6e6395
mossa <martin.ossa.8@gmail.com>
2023-05-18 10:52:51 +0200

    Update decorator sample

--- decorator_sample.rb
+++ decorator_sample.rb
@@ -3,10 +3,19 @@ require_relative 'nameable'
 require_relative 'base_decorator'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'classroom'
+require_relative 'student'
+require_relative 'book'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+
+def run_example
+  person = Person.new(22, 'maximilianus')
+  puts person.correct_name
+  capitalized_person = CapitalizeDecorator.new(person)
+  puts capitalized_person.correct_name
+  capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+  puts capitalized_trimmed_person.correct_name
+end
+
+run_example
+
