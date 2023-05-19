c8beaf0b56a2ebbf7946858188f84e279fa8a3c1
mossa <martin.ossa.8@gmail.com>
2023-05-19 10:17:09 +0200

    Fix add_rental function

--- person.rb
+++ person.rb
@@ -22,9 +22,8 @@ class Person < Nameable
     @name
   end
 
-  def add_rental(rental)
-    @rentals << rental
-    rental.book.add_rental(rental)
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
