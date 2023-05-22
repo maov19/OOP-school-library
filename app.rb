b6b9412c8fcdb9f0ad07223fc60c73dc609dc5d1
mossa <martin.ossa.8@gmail.com>
2023-05-22 14:48:43 +0200

    Update app file with classes refactor

--- app.rb
+++ app.rb
@@ -47,18 +47,3 @@ class App
     puts '0. Quit'
   end
 end
-
-def main
-  books = [
-    Book.new('Book 1', 'Author 1')
-  ]
-
-  people = [
-    Student.new(18, 'Classroom 1', 'Alice')
-  ]
-
-  app = App.new(books, people)
-  app.start
-end
-
-main
