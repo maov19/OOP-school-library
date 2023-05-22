8c7f475452c1e62f5747e11837dfe395dd2e056f
mossa <martin.ossa.8@gmail.com>
2023-05-22 14:46:43 +0200

    Add files to models directory

---
+++ models/student.rb
@@ -0,0 +1,19 @@
+require_relative 'person'
+
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(age, classroom, name = 'Unknown')
+    super(age, name)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\\_(ツ)_/¯'
+  end
+
+  def classroom_association=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
