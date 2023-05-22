8f15dec9b7b7eda64ed9bcaea9c8e4ea09e7f5e5
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-22 16:02:38 +0300

    ListBooksAction class to list all books

---
+++ actions/list_books.rb
@@ -0,0 +1,23 @@
+list_books.rb
+require_relative '../models/person'
+require_relative '../models/student'
+require_relative '../models/teacher'
+require_relative '../models/classroom'
+require_relative '../models/book'
+require_relative '../models/rentals'
+
+# 1. List all books
+class ListBooksAction
+  def initialize(books)
+    @books = books
+  end
+
+  def execute
+    puts '----------------------------------------'
+    puts 'All Books:'
+    @books.each do |book|
+      puts "- #{book.title} by #{book.author}"
+      puts '----------------------------------------'
+    end
+  end
+end
