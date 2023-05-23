311ba76c114c9537ea2035474b57f33bbd02d8cd
mossa <martin.ossa.8@gmail.com>
2023-05-23 17:35:24 +0200

    Update book class

--- models/book.rb
+++ models/book.rb
@@ -1,4 +1,5 @@
 require_relative 'rentals'
+require 'json'
 
 class Book
   attr_accessor :title, :author, :rentals
@@ -11,5 +12,23 @@ class Book
 
   def add_rental(person, date)
     Rental.new(date, self, person)
+  end
+
+  def to_json(*_args)
+    {
+      'title' => @title,
+      'author' => @author,
+      'rentals' => @rentals.map(&:to_json)
+    }.to_json
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    book = Book.new(data['title'], data['author'])
+    data['rentals'].each do |rental_json|
+      rental = Rental.from_json(rental_json)
+      book.rentals << rental
+    end
+    book
   end
 end
