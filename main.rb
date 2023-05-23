7248250a29e16af39bda45894cdc353efaae0a6e
mossa <martin.ossa.8@gmail.com>
2023-05-23 17:36:42 +0200

    Update main and app files for data storage

--- main.rb
+++ main.rb
@@ -2,14 +2,16 @@ require_relative 'app'
 
 def main
   books = [
-    Book.new('Book 1', 'Author 1')
+    # Book.new('Book 1', 'Author 1')
   ]
 
   people = [
-    Student.new(18, 'Classroom 1', 'Alice')
+    # Student.new(18, 'Classroom 1', 'Alice')
   ]
 
-  app = App.new(books, people)
+  rentals = []
+
+  app = App.new(books, people, rentals)
   app.start
 end
 
