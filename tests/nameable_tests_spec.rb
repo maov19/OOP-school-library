d7f2c9fcc2d1b83bda4dc46b7066b699ca72ba1d
mossa <martin.ossa.8@gmail.com>
2023-05-24 18:01:40 +0200

    Add unit test for nameable class

---
+++ tests/nameable_tests_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require_relative '../utils/nameable'
+
+class SampleClass < Nameable
+end
+
+describe Nameable do
+    let (:nameable) {SampleClass.new}
+
+  describe '#correct_name' do
+    it 'raises NotImplementedError' do
+      expect { nameable.correct_name }.to raise_error(NotImplementedError)
+    end
+  end
+end
