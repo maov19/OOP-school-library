3a6b8abc34830f1fa65170cc72d6a87c2234c55c
mossa <martin.ossa.8@gmail.com>
2023-05-24 18:01:11 +0200

    Add unit test for capitalize_decorator class

---
+++ tests/capitalize_decorator_test_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require_relative '../utils/capitalize_decorator'
+
+class MockNameable
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def correct_name
+    name
+  end
+end
+
+describe CapitalizeDecorator do
+  let(:nameable) { MockNameable.new('john doe') }
+  let(:decorator) { CapitalizeDecorator.new(nameable) }
+
+  describe '#correct_name' do
+    it 'capitalizes the name returned by the decorated object' do
+      expect(decorator.correct_name).to eq('John doe')
+    end
+  end
+end
