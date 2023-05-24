7abc92729282ac7b7fe6e71e2d665f3806fba65a
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-24 23:33:04 +0300

    Add teacher test file

---
+++ tests/teacher_tests_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require 'json'
+require_relative '../models/rentals'
+require_relative '../models/book'
+require_relative '../models/person'
+require_relative '../models/teacher'
+
+describe Student do
+
+  before :each do
+      @teacher = Teacher.new(23, "Computer", "John")
+  end
+  
+  describe "#new" do
+    it "returns an Teacher object belong to Teacher class" do
+      expect(@teacher).to be_an_instance_of Teacher
+    end
+  end
+
+  describe "#age" do
+      it "return teacher age" do
+          expect(@teacher.age).to eql 23
+      end
+  end
+
+  describe "#name" do
+      it "return teacher name" do
+          expect(@teacher.name).to eql "John"
+      end
+  end
+
+  describe "#subject" do
+  it "returns the subject of the Teacher" do
+    expect(@teacher.subject).to eq("Computer")
+      end
+  end
+
+  describe "#can user the services" do
+    it "returns true" do
+        expect(@teacher.can_use_services?).to be true
+    end
+  end
+
+end
