18fea90cb360ac06cf788b6084990e9943467ef2
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-24 13:17:07 +0300

    Removing commented code/to make code clean

--- models/rentals.rb
+++ models/rentals.rb
@@ -9,31 +9,6 @@ class Rental
     person.rentals << self
   end
 
-  # def to_json(*_args)
-  #   {
-  #     'date' => @date,
-  #     'book' => {
-  #       'title' => @book.title,
-  #       'author' => @book.author
-  #     },
-  #     'person' => {
-  #       'name' => @person.name,
-  #       'age' => @person.age
-  #     }
-  #   }.to_json
-  # end
-
-
-  # def self.from_json(json)
-  #   data = JSON.parse(json)
-  #   date = data['date']
-  #   book_data = data['book']
-  #   person_data = data['person']
-  #   book = Book.new(book_data['title'], book_data['author'])
-  #   person = Person.new(person_data['name'], person_data['age'])
-  #   Rental.new(date, book, person)
-  # end
-
   def to_json(*_args)
     {
       'date' => @date,
@@ -47,17 +22,6 @@ class Rental
       }
     }.to_json
   end
-
-
-  # def self.from_json(json)
-  #   data = JSON.parse(json)
-  #   date = data['date']
-  #   book_data = data['book']
-  #   person_data = data['person']
-  #   book = Book.from_json(book_data)
-  #   person = Person.from_json(person_data)
-  #   Rental.new(date, book, person)
-  # end
 
   def self.from_json(json)
     data = JSON.parse(json)
