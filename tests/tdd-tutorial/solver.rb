55539ddfd923634a164ec967db2c42e86c55c191
Martin Ossa <martin.ossa.8@gmail.com>
2023-05-25 13:28:29 +0200

    Fix styling

--- tests/tdd-tutorial/solver.rb
+++ tests/tdd-tutorial/solver.rb
@@ -1,17 +1,11 @@
-# second commit - add factorial method
-# Create a method called factorial that takes one argument, an integer N, and returns the factorial for that number.
-# The factorial is the multiplication of all integers from 1 to N and has the special case that the factorial of 0 is 1.
-# This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception.
 class Solver
   def factorial(num)
     return 'No factorial for negative integers' if num.negative?
-
 
     sum = 1
     (1..num).each do |i|
       sum *= i
     end
-
 
     sum
   end
