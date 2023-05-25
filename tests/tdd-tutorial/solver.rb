bef7df58c40162fbebfa6fdc01a7c8d03a613039
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-25 16:11:51 +0300

    Add fizzbuz class,method

--- tests/tdd-tutorial/solver.rb
+++ tests/tdd-tutorial/solver.rb
@@ -22,6 +22,14 @@ class Solver
 
     reversed
   end
+
+  def fizzbuzz(n)
+    return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
+    return 'fizz' if (n % 3).zero?
+    return 'buzz' if (n % 5).zero?
+
+    n.to_s
+  end
 end
 
 string = Solver.new
