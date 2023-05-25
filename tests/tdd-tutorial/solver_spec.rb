5ca1c125b3291ffe42d6bc02b96175e6528b6e10
mossa <martin.ossa.8@gmail.com>
2023-05-25 12:43:59 +0200

    Add test for factorial method

--- tests/tdd-tutorial/solver_spec.rb
+++ tests/tdd-tutorial/solver_spec.rb
@@ -1,6 +1,20 @@
-require 'rspec'  
+require 'rspec'
 require_relative 'solver'
 
 describe Solver do
-    
+  describe '#factorial' do
+    it 'returns 120' do
+      five = Solver.new
+      expect(five.factorial(5)).to eq(120)
+    end
+    it 'returns "No factorial for negative integers"' do
+        negative = Solver.new
+        expect(negative.factorial(-1)).to eq("No factorial for negative integers")
+    end
+  end
+
 end
+
+
+
+# This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception.
